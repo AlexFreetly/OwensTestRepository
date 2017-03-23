a8a6866e9a2dd8d3ab0fdaf385bd8aa805d969c7
OwenSchleicher <6579001@isd535.org>
2017-03-23 12:39:57 -0500

    Add files via upload

---
+++ flow.py
@@ -0,0 +1,27 @@
+def f(x):
+    n=x 
+    if int(n)==n:
+        if n%2==0:
+            if n%3==0:
+                return ('is a multiple of 6')
+            else: 
+                return ('is even')
+        else: 
+            return ('is odd')
+    else:
+        return ('is not an integer')
+                
+            
+def f_test():
+    works = True
+    if f(12) != 'is a multiple of 6':
+            works = False
+    if f(5) != 'is odd':
+            works= False
+    if f(2) != 'is even':
+            works = False 
+    if works==True: 
+        print ('works=true')
+    else: 
+        print ('works=false')
+        
